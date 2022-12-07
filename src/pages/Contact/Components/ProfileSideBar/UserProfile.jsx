1f5e4cb674653ce1f0698a55800a235f526160f1
SWS-5007 <superwebstar5007@gmail.com>
2022-12-08 05:01:29 +0900

    Fixed the clickout of userprofile

--- src/pages/Contact/Components/ProfileSideBar/UserProfile.jsx
+++ src/pages/Contact/Components/ProfileSideBar/UserProfile.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react"
-import { Avatars } from "../../../../assets"
-import { BiPhoneCall, BiMessageAltDetail, BiMailSend } from "react-icons/bi"
-import { FiMoreHorizontal } from "react-icons/fi"
+import React, { useState, useEffect } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
+import { Avatars } from "../../../../assets";
+import { BiPhoneCall, BiMessageAltDetail, BiMailSend } from "react-icons/bi";
+import { FiMoreHorizontal } from "react-icons/fi";
 
-import "../../index.css"
+import "../../index.css";
 
 export const UserProfile = (props) => {
   return (
@@ -15,23 +16,54 @@ export const UserProfile = (props) => {
             {props.selectedContact.initials}
           </span>
         </span>
-        
+
         <div>
-          {props.selectedIndex == true ?
-            <div className="style-editName style-darkShadow" onBlur={props.handleDivBlur}>
-              <label className="text-darkGrayText text-[13px] font-[450]">First name</label>
-              <input id="edit-first-name" name="first_name" className="text-black dark:text-white style-editNameInput " placeholder="Add a first name..."
-                defaultValue={props.selectedContact.first_name}
-                onKeyDown={props.handleKeyDown} />
-              <label className="text-darkGrayText text-[13px] font-[450]">Last name</label>
-              <input id="edit-last-name" name="last_name" className="text-black dark:text-white style-editNameInput" placeholder="Add a last name..."
-                defaultValue={props.selectedContact.last_name}
-                onKeyDown={props.handleKeyDown} />
-            </div> :
-            <div className="outline-none text-center cursor-pointer text-2xl font-bold my-[15px]" onClick={() => { props.setSelectedIndex(true) }}>
-              {props.selectedContact.first_name + ' ' + props.selectedContact.last_name}
+          {props.selectedIndex == true ? (
+            <OutsideClickHandler
+              onOutsideClick={() => {
+                props.handleDivBlur;
+              }}
+            >
+              <div
+                className="style-editName style-darkShadow"
+                onBlur={props.handleDivBlur}
+              >
+                <label className="text-darkGrayText text-[13px] font-[450]">
+                  First name
+                </label>
+                <input
+                  id="edit-first-name"
+                  name="first_name"
+                  className="text-black dark:text-white style-editNameInput "
+                  placeholder="Add a first name..."
+                  defaultValue={props.selectedContact.first_name}
+                  onKeyDown={props.handleKeyDown}
+                />
+                <label className="text-darkGrayText text-[13px] font-[450]">
+                  Last name
+                </label>
+                <input
+                  id="edit-last-name"
+                  name="last_name"
+                  className="text-black dark:text-white style-editNameInput"
+                  placeholder="Add a last name..."
+                  defaultValue={props.selectedContact.last_name}
+                  onKeyDown={props.handleKeyDown}
+                />
+              </div>
+            </OutsideClickHandler>
+          ) : (
+            <div
+              className="outline-none text-center cursor-pointer text-2xl font-bold my-[15px]"
+              onClick={() => {
+                props.setSelectedIndex(true);
+              }}
+            >
+              {props.selectedContact.first_name +
+                " " +
+                props.selectedContact.last_name}
             </div>
-          }
+          )}
         </div>
 
         <div className="flex items-center justify-center pb-8 border-b border-gray-300 dark:border-gray-600">
@@ -50,5 +82,5 @@ export const UserProfile = (props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
