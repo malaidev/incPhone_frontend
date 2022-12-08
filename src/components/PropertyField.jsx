dc7e0da9fc22e036012e52bf17516b3f98b622bf
malaidev <jefferykang921015@gmail.com>
2022-12-08 13:54:49 +0900

    Added the EditTitleFunction

--- src/components/PropertyField.jsx
+++ src/components/PropertyField.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
+
 import { Avatars } from "../assets";
 import ToastAlert from "./ToastAlert";
 import PhoneNumber from "./PhoneNumber";
@@ -15,6 +17,8 @@ const propertiesOrderObject = {
 
 const Property = (props) => {
   const [toastStatus, setToast] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
+  const [editTitleId, setEditTitleId] = useState();
 
   const propertyName = props.item[0];
   const propertyValue = props.item[1];
@@ -23,7 +27,34 @@ const Property = (props) => {
     ? props.subItem[propertiesOrderObject[propertyName]]
     : propertyValue;
 
+  const titleId = propertyValueType
+    ? props.subItem.id
+    : props.selectedContact.id;
+
   const uniqueKey = Math.floor(Math.random() * 100);
+
+  const handleEditTitle = (titleId) => {
+    setEditTitleId(titleId);
+    const propertyDefaultTitle = propertyValueType
+      ? props.subItem.title
+      : propertyName.charAt(0).toUpperCase() + propertyName.slice(1);
+    setNewTitle(propertyDefaultTitle);
+  };
+
+  const handleSaveNewTitle = (titleId) => {
+    const newTitle = document.getElementById(titleId).value;
+    const updateValue = {
+      title: newTitle,
+    };
+    props.handleUpdateProperty(
+      props.selectedContact.address_book_id,
+      props.selectedContact.id,
+      props.subItem.id,
+      propertyName,
+      updateValue
+    );
+    setEditTitleId("");
+  };
 
   const handleUpdate = (e, valueType, subItem, objectKey) => {
     if (e.key === "Enter") {
@@ -66,9 +97,51 @@ const Property = (props) => {
       setToast(false);
     }, 1500);
   };
+
   return (
-    <div className="flex px-[4px]" key={propertyName + uniqueKey}>
-      <div className="flex items-center w-[60%]">
+    <div className="flex relative px-[4px]" key={propertyName + uniqueKey}>
+      {editTitleId === titleId ? (
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setEditTitleId("");
+          }}
+        >
+          <div className="absolute z-[2] w-[120px] h-[100px] bg-[#f3f3f5] dark:bg-[#21212F]  border-solid border-[1px] border-[#4b5563] dark:shadow-[0 2px 20px 0 rgb(0 0 0 / 35%), 0 0 0 1px rgb(80 80 98 / 55%)] rounded-lg">
+            <input
+              id={titleId}
+              className="w-[-webkit-fill-available] my-[5px] px-[15px] py-[3px] border-0 outline-none text-[0.8rem] !bg-transparent "
+              defaultValue={newTitle}
+            />
+            <div>
+              <div
+                className="w-[90%] bg-[#f3f3f5] dark:bg-[#21212f] hover:rounded-lg dark:hover:bg-[#2a2a3c] cursor-pointer h-[30px] px-[10px] m-auto text-[13px] leading-[2]"
+                onClick={() => handleSaveNewTitle(titleId)}
+              >
+                Save
+              </div>
+              <div className="w-[90%] bg-[#f3f3f5] dark:bg-[#21212f] hover:rounded-lg dark:hover:bg-[#2a2a3c] cursor-pointer h-[30px] px-[10px] m-auto text-[13px] leading-[2]">
+                Delete
+              </div>
+            </div>
+          </div>
+        </OutsideClickHandler>
+      ) : (
+        ""
+      )}
+      <div
+        className={`${
+          propertyValueType
+            ? "hover:bg-[#e5e7eb] dark:hover:bg-[#252434] cursor-pointer transition-all duration-500"
+            : ""
+        } flex items-center w-[60%] pl-[0px]  min-h-[30px]  rounded-md text-[15px]`}
+        onClick={() =>
+          propertyValueType
+            ? handleEditTitle(
+                propertyValueType ? props.subItem.id : props.selectedContact.id
+              )
+            : ""
+        }
+      >
         <img
           className="text-black dark:text-white"
           alt="Avatar"
@@ -83,8 +156,7 @@ const Property = (props) => {
             : propertyName.charAt(0).toUpperCase() + propertyName.slice(1)}
         </span>
       </div>
-
-      <div className="flex items-center justify-between w-[100%] hover:bg-[#e5e7eb] dark:hover:bg-[#252434] contactPropertyDiv">
+      <div className="flex relative items-center justify-between w-[100%] hover:bg-[#e5e7eb] dark:hover:bg-[#252434] contactPropertyDiv">
         {(() => {
           switch (propertyName) {
             case "phones":
@@ -120,7 +192,9 @@ const Property = (props) => {
         })()}
 
         <button
-          className={`${propertyDefaultValue ? "flex" : "hidden"} absolute right-[15px]`}
+          className={`${
+            propertyDefaultValue ? "flex" : "hidden"
+          } absolute right-[0px]`}
           value={propertyDefaultValue}
           onClick={(e) => {
             toastAlert();
